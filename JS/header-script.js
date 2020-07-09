7973e2277e1122d346f85b66cf018f09a3f05b4a
Jacoblynch99 <jacoblynch99@gmail.com>
2020-07-09 06:36:39 -0500

    major changes

--- JS/header-script.js
+++ JS/header-script.js
@@ -1,14 +1,11 @@
-
 function handleOpen() {
 const x = document.getElementById("menu")
   if (x.style.display === "block") {
     x.style.display = "none";
+    // document.getElementById("menu").style
   } else {
     x.style.display = "block";
   }
-
-  
-
 }
 
 function handleClose() {
